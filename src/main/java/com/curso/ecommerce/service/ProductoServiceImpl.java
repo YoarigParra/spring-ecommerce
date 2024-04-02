14df0d581762b9ae74e440306fd128300096e328
Yoarigparra <parrayoarig@gmail.com>
2024-04-02 19:45:41 -0400

    Funcionalidad de mostrar productos para el administrador

--- src/main/java/com/curso/ecommerce/service/ProductoServiceImpl.java
+++ src/main/java/com/curso/ecommerce/service/ProductoServiceImpl.java
@@ -1,5 +1,6 @@
 package com.curso.ecommerce.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,26 +13,31 @@ import com.curso.ecommerce.repository.IProductoRepository;
 public class ProductoServiceImpl implements IProductoService {
 	
 	@Autowired
-	private IProductoRepository productorepository;
+	private IProductoRepository productoRepository;
 
 	@Override
 	public Producto save(Producto producto) {
-		return productorepository.save(producto);
+		return productoRepository.save(producto);
 	}
 
 	@Override
 	public Optional<Producto> get(Integer id) {
-		return productorepository.findById(id);
+		return productoRepository.findById(id);
 	}
 
 	@Override
 	public void update(Producto producto) {
-		productorepository.save(producto);
+		productoRepository.save(producto);
 	}
 
 	@Override
 	public void delate(Integer id) {
-	   productorepository.deleteById(id);
+	   productoRepository.deleteById(id);
+	}
+
+	@Override
+	public List<Producto> findAll() {
+		return productoRepository.findAll();
 	}
 
 }
