47e92b16fb7413ce34ed7da4226c329b19ca4c92
Yoarigparra <parrayoarig@gmail.com>
2024-04-03 19:53:41 -0400

    Implementacion de las funcionalidades de cargar y mostrar imagen para el administrador y clase gestora de recursos estaticos

--- src/main/java/com/curso/ecommerce/controller/AdministradorController.java
+++ src/main/java/com/curso/ecommerce/controller/AdministradorController.java
@@ -1,16 +1,32 @@
 package com.curso.ecommerce.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.curso.ecommerce.model.Producto;
+import com.curso.ecommerce.service.IProductoService;
 
 @Controller
 @RequestMapping("/administrador")
 public class AdministradorController {
     
+    @Autowired
+	private IProductoService productosService;
+	
+	
+	
 	@GetMapping("")
-	public String home() {
-         
+	public String home(Model model) {
+
+		List<Producto> productos=productosService.findAll();
+		model.addAttribute("productos", productos);
+		
+		
 		return "administrador/home";
 	}
 }
--- src/main/java/com/curso/ecommerce/controller/ProductoController.java
+++ src/main/java/com/curso/ecommerce/controller/ProductoController.java
@@ -54,15 +54,7 @@ public class ProductoController {
 			producto.setImagen(nombreImagen);
 			
 		}else {
-			
-			if(file.isEmpty()) {//Cuando editamos el producto pero no cambiamos la imagen
-				Producto p=new Producto();
-				p=productoService.get(producto.getId()).get();
-				producto.setImagen(p.getImagen());
-			}else {
-				String nombreImagen=upload.saveImage(file);
-				producto.setImagen(nombreImagen);
-			}
+		
 			
 		}
 		
@@ -82,13 +74,41 @@ public class ProductoController {
 	}
 	
 	@PostMapping("/update")
-	public String update(Producto producto){
+	public String update(Producto producto, @RequestParam("img") MultipartFile file) throws IOException{
+		
+		Producto p=new Producto();
+		p=productoService.get(producto.getId()).get();
+		
+		if(file.isEmpty()) {//Cuando editamos el producto pero no cambiamos la imagen
+			
+			producto.setImagen(p.getImagen());
+		}else {//cuando se edita tambien la imagen
+			
+		
+			//eliminar cuando no sea la imagen por defecto
+			if(!p.getImagen().equals("default.jpg")) upload.deleteImage(p.getImagen());
+				
+			
+			String nombreImagen=upload.saveImage(file);
+			producto.setImagen(nombreImagen);
+		}
+		
+		producto.setUsuario(p.getUsuario());
+		
+		
 		productoService.update(producto);
 		return "redirect:/productos";
 	}
 	
 	@GetMapping("/delete/{id}")
 	public String delete(@PathVariable Integer id){
+		Producto p =new Producto();
+		p=productoService.get(id).get();
+		
+		//eliminar cuando no sea la imagen por defecto
+		if(!p.getImagen().equals("default.jpg")) upload.deleteImage(p.getImagen());
+			
+		
 		productoService.delate(id);
 		return "redirect:/productos";
 	}
