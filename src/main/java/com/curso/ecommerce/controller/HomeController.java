bcc6789fba318db75b38b0985770f4ac9e99d5ca
Yoarigparra <parrayoarig@gmail.com>
2024-04-10 11:29:01 -0400

    Funcionalidad para mostrar el producto al usuario

--- src/main/java/com/curso/ecommerce/controller/HomeController.java
+++ src/main/java/com/curso/ecommerce/controller/HomeController.java
@@ -1,16 +1,22 @@
 package com.curso.ecommerce.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 
 import com.curso.ecommerce.service.IProductoService;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
+	
+	private final Logger logger=LoggerFactory.getLogger(HomeController.class);
 
     @Autowired
 	private IProductoService productoService; 
@@ -21,5 +27,13 @@ public class HomeController {
 		model.addAttribute("productos", productoService.findAll());
 		
 		return "usuario/home";
+	}
+	
+	@GetMapping("productoHome/{id}")
+	public String productoHome(@PathVariable Integer id) {
+		
+		logger.info("Id del producto enviado {}",id);
+		
+		return "usuario/productohome";
 	}
 }
